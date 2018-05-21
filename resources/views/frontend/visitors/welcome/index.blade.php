546bdabc7132892cb9de548facb5d8104ddf9990
Gabriel Liseanu <G052215@gira.de>
2018-05-21 22:55:20 +0200

    exchanged thumbs images. added mars .img

--- resources/views/frontend/visitors/welcome/index.blade.php
+++ resources/views/frontend/visitors/welcome/index.blade.php
@@ -5,26 +5,22 @@
         <div class="container">
             <div class="heading-text">
                 <h1 class="animated flipInY delay1 start-quiz">
-                    Start your Mars Quiz
+                    <b>Mars Quiz</b>
                 </h1>
                 <p class="start-quiz">
-                    Free Online education template for elearning and online education institute.
+                    <b>Teste Dein Wissen über den Mars, und gewinne einer der 3 Preise !</b>                    
+                    <p> 1. Preis: Ein Amazon Gutschein in Wert von 25 Euro</p>
+                    <p>2. Preis: Ein Amazon Gutschein in Wert von 20 Euro</p>
+                    <p>3. Preis: Ein Amazon Gutschein in Wert von 15 Euro</p>
                 </p>
             </div>
 
             <div class="fluid_container">
                 <div class="camera_wrap camera_emboss pattern_1" id="camera_wrap_4">
-                    <div data-thumb="images/slides/thumbs/img1.jpg" data-src="images/slides/img1.jpg">
-                        <h2>
-                            We develop.
-                        </h2>
-                    </div> 
-
-                    <div data-thumb="images/slides/thumbs/img2.jpg" data-src="images/slides/img2.jpg">
-                    </div>
-
-                    <div data-thumb="images/slides/thumbs/img3.jpg" data-src="images/slides/img3.jpg">
-                    </div>
+                    <div data-thumb="images/slides/thumbs/img1.jpg" data-src="images/slides/mars01.jpg"></div> 
+                    <div data-thumb="images/slides/thumbs/img2.jpg" data-src="images/slides/mars02.jpg"></div>
+                    <div data-thumb="images/slides/thumbs/img3.jpg" data-src="images/slides/mars03.jpg"></div>
+                    <div data-thumb="images/slides/thumbs/img4.jpg" data-src="images/slides/mars04.jpg"></div>                    
                 </div>
             </div>        
         </div>
